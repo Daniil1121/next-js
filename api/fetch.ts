5e2234fcc7c5f1e2ad41497fa9ca735dca244d28
Daniil <motunov1121@yandex.ru>
2023-01-21 10:57:14 +0300

    refactoring

--- api/fetch.ts
+++ api/fetch.ts
@@ -1,12 +1,15 @@
 import { IFormValues } from "../next.d.interfaces";
 
 export const fetchFunc = async (data: IFormValues) => {
-  let result = fetch("http://localhost:3004/feedback", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  });
-  if ((await result).status === 201) {
-    return true;
-  } else return false;
+  try {
+    let result = fetch("http://localhost:3004/feedback", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    if ((await result).status === 200 || (await result).status === 201)
+      return true;
+  } catch (error) {
+    return false;
+  }
 };
