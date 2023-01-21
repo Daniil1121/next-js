88fd09a3f3ce9aff803d0ce105d9a954dfa64720
Daniil <motunov1121@yandex.ru>
2023-01-21 12:01:30 +0300

    add nav tag

--- components/Footer/components/Socials.tsx
+++ components/Footer/components/Socials.tsx
@@ -7,28 +7,30 @@ const Socials = () => {
   return (
     <div className="footer__socials">
       <h4 className="footer__socials__title footer__title">Social media</h4>
-      <ul className="list-reset footer__socials__list">
-        <li className="footer__socials__item">
-          <a target="_blank" href="https://ru-ru.facebook.com/">
-            <FaceBookSVG />
-          </a>
-        </li>
-        <li className="footer__socials__item">
-          <a target="_blank" href="https://www.mozilla.org/ru/firefox/">
-            <FoxSVG />
-          </a>
-        </li>
-        <li className="footer__socials__item">
-          <a target="_blank" href="https://twitter.com/">
-            <TwitterSVG />
-          </a>
-        </li>
-        <li className="footer__socials__item">
-          <a target="_blank" href="http://www.linkedin.com/">
-            <LinkedInSVG />
-          </a>
-        </li>
-      </ul>
+      <nav>
+        <ul className="list-reset footer__socials__list">
+          <li className="footer__socials__item">
+            <a target="_blank" href="https://ru-ru.facebook.com/">
+              <FaceBookSVG />
+            </a>
+          </li>
+          <li className="footer__socials__item">
+            <a target="_blank" href="https://www.mozilla.org/ru/firefox/">
+              <FoxSVG />
+            </a>
+          </li>
+          <li className="footer__socials__item">
+            <a target="_blank" href="https://twitter.com/">
+              <TwitterSVG />
+            </a>
+          </li>
+          <li className="footer__socials__item">
+            <a target="_blank" href="http://www.linkedin.com/">
+              <LinkedInSVG />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
